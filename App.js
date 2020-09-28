39fc50e4b4424ed33bcbaeb442390d12aaf08193
Marcelo <marcelobiagiola01@gmail.com>
2020-09-28 09:38:03 -0400

    add thins to the dom

--- App.js
+++ App.js
@@ -1,43 +1,76 @@
-//import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StatusBar, Platform, Alert, Button, StyleSheet, Text, View, Image, TouchableOpacity, TouchableNativeFeedback} from 'react-native';
-import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks'
-import Part1 from './part1'
-import Part2 from './part2'
-import Dimensions from './dimentions'
-import FlexBoxesRow from './flexBoxesRow'
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, FlatList, TouchableWitOutOpacity } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('')
+  const [courseGoals, setCourseGoals] = useState([])
+  
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText)
+  }
+
+  const addGoalHandler = () => {
+    /* setCourseGoals([...courseGoals, enteredGoal]) */
+    /* setCourseGoals(currentGoal => [...courseGoals, enteredGoal]) */
+    setCourseGoals(currentGoals => [...currentGoals, 
+    { id: Math.random().toString(), value: enteredGoal }
+    ])
+  }
+
+
+  const deletedGoalHandler = () => {
+
+  }
+
   return (
-    <View style={styles.container}>
-      {/* <Part1/>
-      <Part2/>
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          width: "100%",
-          height: landscape ? "100%" : "50%"
-        }}
-      ></View>
-      <View
-        style={{
-          backgroundColor: 'tomato',
-          flex: 0.5,
-          width: 50,
-          height: 50
-        }}
-      ></View> 
-      <Dimensions />*/}
-      <FlexBoxesRow />
-    </View>
-  );
+    <View style={styles.screen}>
+      <View style={styles.inputContainer} >
+        <TextInput 
+          placeholder='Course Goal' 
+          style={styles.input} 
+          onChangeText={goalInputHandler}  
+          value={enteredGoal}
+        />
+        <Button title='add' onPress={addGoalHandler} />
+      </View>
+
+      <FlatList 
+        /*keyExtractor in case we use id insted key value*/
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={ itemData =>  (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
+      />
+        
+      
+    </View> 
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    paddingTop: 20,
+    alignItems: 'center' 
   },
-});
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%', 
+    borderColor: 'black',
+    padding: 10,
+    borderWidth: 1
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
+  }
+})
